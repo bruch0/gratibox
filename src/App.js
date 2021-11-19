a8887950222fec1619532b5bcb65cc367ab839bc
bruch0 <lucas.bruch0@gmail.com>
2021-11-19 13:50:28 -0300

    fix: eslint deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/function-component-definition */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './shared/GlobalStyles';
